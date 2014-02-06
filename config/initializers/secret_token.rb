4880f2ecc7fca5abb35e09916ea03aae5cbc32bb
it gone <damnadmster@gmail.com>
2014-02-06 12:15:30 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '31142a400ae1ac846cb211d79e4d98ec3d58484ac5196fe4245b5343433e225c49a2d04916dbbd9452889936a6a0edcc5d4519a04ed4d9eda3861733524d75be'
